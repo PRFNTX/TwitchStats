e12304735a2d6585ae7acac8369275be016a9204
Brent <prfntx.treme@gmail.com>
2018-01-18 14:37:52 -0500

    summary view map

--- src/pages/view/summary.js
+++ src/pages/view/summary.js
@@ -4,19 +4,20 @@ import PropTypes from 'prop-types'
 class Summary extends Component{
     render(){
         console.log(this.props.summary)
+        let items = Object.keys(this.props.summary).map(val=>{
+            return(
+                <tr>
+                    <td>val</td>
+                    <td>{this.props.summary[val]}</td>
+                </tr>
+            )
+        })
         return(
             <div>
                 <h4>Summary for Session: {this.props.summary.sessionId}</h4>
                 <table>
                     <tbody>
-                        <tr>
-                            <td>Unique Views</td>
-                            <td>{this.props.summary.uniqueViews}</td>
-                        </tr>
-                        <tr>
-                            <td>Viewer Retention (average minutes watched)</td>
-                            <td>{this.props.summary.viewerRetention}</td>
-                        </tr>
+                        {items}
                     </tbody>
                 </table>
                 <button onClick={()=>this.props.summarize(this.props.summary.sessionId)}> Recalculate </button>
